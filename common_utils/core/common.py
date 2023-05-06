485684938b2fe2535c73693ce6a5f2782cb0f9ab
gao-hongnan <hongnangao@gmail.com>
2023-05-06 14:58:56 +0800

    feat: add load_dict to core.common.py.

--- common_utils/core/common.py
+++ common_utils/core/common.py
@@ -1,14 +1,35 @@
 import json
 import os
 import random
-from typing import Dict, Optional, Type
+from typing import Dict, Optional, Type, Any
 
 import numpy as np
 import torch
 
 
+def load_dict(filepath: str) -> Dict[str, Any]:
+    """Load a dictionary from a JSON's filepath.
+
+    Parameters
+    ----------
+    filepath : str
+        Location of the JSON file.
+
+    Returns
+    -------
+    Dict[str, Any]
+        Dictionary loaded from the JSON file.
+    """
+    with open(filepath) as fp:
+        d = json.load(fp)
+    return d
+
+
 def save_dict(
-    d: Dict, filepath: str, cls: Optional[Type] = None, sort_keys: bool = False
+    d: Dict[str, Any],
+    filepath: str,
+    cls: Optional[Type] = None,
+    sort_keys: bool = False,
 ) -> None:
     """
     Save a dictionary to a specific location.
@@ -39,23 +60,23 @@ def seed_all(seed: Optional[int] = 1992, seed_torch: bool = True) -> None:
         Seed number to be used, by default 1992.
     seed_torch : bool, optional
         Whether to seed PyTorch or not, by default True.
-
     """
     print(f"Using Seed Number {seed}")
 
-    # set PYTHONHASHSEED env var at fixed value
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)  # for numpy pseudo-random generator
-    # set fixed value for python built-in pseudo-random generator
-    random.seed(seed)
+    # fmt: off
+    os.environ["PYTHONHASHSEED"] = str(seed) # set PYTHONHASHSEED env var at fixed value
+    np.random.seed(seed)                     # numpy pseudo-random generator
+    random.seed(seed)                        # python's built-in pseudo-random generator
+
 
     if seed_torch:
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-        torch.cuda.manual_seed(seed)  # pytorch (both CPU and CUDA)
+        torch.cuda.manual_seed(seed)         # pytorch (both CPU and CUDA)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.enabled = False
+    # fmt: on
 
 
 def seed_worker(_worker_id: int, seed_torch: bool = True) -> None:
