fc7f5be44b97969d4f47cd42563efca552c53223
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-05-09 12:23:57 +0800

    scripts: refactor make_pbs.sh.

--- scripts/make_pbs.sh
+++ scripts/make_pbs.sh
@@ -40,54 +40,54 @@ function create_pbs_script() {
         exit 1
     fi
 
-    cat <<EOF > run_mlflow.pbs
-    #!/bin/bash
-    #PBS -N run_mlflow
-    #PBS -l select=1:ncpus=$ncpus:mem=$mem
-    #PBS -l walltime=$walltime
-    #PBS -q $queue
-    #PBS -koed
-    #PBS -o /home/users/$username/$log_dir
-    #PBS -e /home/users/$username/$log_dir
-    #PBS -P $project_id
+    cat << EOF > job.pbs
+#!/bin/bash
+#PBS -N run_mlflow
+#PBS -l select=1:ncpus=${ncpus}:mem=${mem}
+#PBS -l walltime=${walltime}
+#PBS -q ${queue}
+#PBS -koed
+#PBS -o /home/users/${username}/${log_dir}
+#PBS -e /home/users/${username}/${log_dir}
+#PBS -P ${project_id}
 
-    print_hostname() {
-        echo "Print Hostname"
-        hostname
-    }
+print_hostname() {
+    echo "Print Hostname"
+    hostname
+}
 
-    print_modules() {
-        echo -e "\n\n Which module in this job."
-        module list 2>&1
-    }
+print_modules() {
+    echo -e "\\n\\n Which module in this job."
+    module list 2>&1
+}
 
-    print_environment() {
-        echo -e "\n\n what is the environment."
-        printenv
-    }
+print_environment() {
+    echo -e "\\n\\n what is the environment."
+    printenv
+}
 
-    print_working_folder() {
-        cd \${PBS_O_WORKDIR}
-        echo -e "my work folder is \$PWD\n\n"
-        echo
-    }
+print_working_folder() {
+    cd \${PBS_O_WORKDIR}
+    echo -e "my work folder is \$PWD\\n\\n"
+    echo
+}
 
-    run_job() {
-        echo The job \$PBS_JOBNAME is running on \`cat \$PBS_NODEFILE\`.
-        hostname > /home/users/$username/$log_dir/\${PBS_JOBID}_\${PBS_JOBNAME}.txt 2>&1
-    }
+run_job() {
+    echo The job \${PBS_JOBNAME} is running on \`cat \$PBS_NODEFILE\`.
+    hostname > /home/users/${username}/${log_dir}/\${PBS_JOBID}_\${PBS_JOBNAME}.txt 2>&1
+}
 
-    source run_mlflow.sh
+source run_mlflow.sh
 
-    # Execute functions
-    cd \$PBS_O_WORKDIR
-    print_hostname
-    print_modules
-    print_environment
-    print_working_folder
-    run_job
-    start_mlflow_server
-    run_mlflow
+# Execute functions
+cd \$PBS_O_WORKDIR
+print_hostname
+print_modules
+print_environment
+print_working_folder
+run_job
+start_mlflow_server
+run_mlflow
 EOF
 }
 
