6633aef9d9fcc044b4397e45bc73e942656724ab
gao-hongnan <hongnangao@gmail.com>
2023-05-14 18:37:06 +0800

    scripts: update content on dvc scripts.

--- scripts/mlops/dvc/dvc_add_push.sh
+++ scripts/mlops/dvc/dvc_add_push.sh
@@ -1,20 +1,28 @@
-check_data_file() {
-    if [[ -z "${DATA_FILE}" ]]; then
-        echo "Error: Data file not provided"
-        usage
-    fi
-}
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m' # No Color
+
+# This script serves as a template to guide you through the process of tracking data with DVC and pushing it to a remote storage.
+# This script is not intended to be run directly. Instead, please refer to the steps outlined within and adapt them to your specific needs.
+
 track_data_file_with_dvc() {
-    dvc add "${DATA_FILE}"
-    DATA_DVC_FILE="${DATA_FILE}.dvc"
-    git add "${DATA_DVC_FILE}"
-    git commit -m "Track data file with DVC"
+    echo -e "${GREEN}Step 1: Track your data file with DVC${RESET}"
+    echo -e "Replace 'DATA_FILE' with the path to your actual data file"
+    echo -e "${YELLOW}> dvc add DATA_FILE${RESET}"
+    echo -e "${YELLOW}> git add DATA_FILE.dvc${RESET}"
+    echo -e "${YELLOW}> git commit -m \"Track data file with DVC\"${RESET}"
+    echo
 }
 
 push_data_to_remote() {
-    if [[ -n "${REMOTE_URL}" && -n "${BUCKET}" && -n "${PATH}" ]]; then
-        dvc push
-    fi
+    echo -e "${GREEN}Step 2: Push your data to the remote storage${RESET}"
+    echo -e "${YELLOW}> dvc push${RESET}"
+    echo
 }
+
+# Display the steps
 track_data_file_with_dvc
 push_data_to_remote
--- scripts/mlops/dvc/dvc_setup_gcs.sh
+++ scripts/mlops/dvc/dvc_setup_gcs.sh
@@ -6,28 +6,39 @@
 
 set -e
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m' # No Color
+
 usage() {
-    if [[ -z "${REMOTE_NAME}" || -z "${REMOTE_URL}" || -z "${BUCKET}" || -z "${PATH}" ]]; then
+    echo "Usage: $0 -d remote_name -r remote_url -b bucket_name -p bucket_path -c credentials"
+    echo
+    echo "Options:"
+    echo "  -d REMOTE_NAME   The name of the remote in DVC (required)"
+    echo "  -r REMOTE_URL    The remote storage URL (e.g., s3://, gs://, azure://) (required)"
+    echo "  -b BUCKET_NAME   The bucket name for the remote storage (required)"
+    echo "  -p BUCKET_PATH   The path within the bucket for the remote storage (required)"
+    echo "  -c CREDENTIALS   The path to the credentials file for the remote storage (required)"
+    echo "  -h               Display this help and exit"
+}
+
+check_required_args() {
+    if [[ -z "${REMOTE_NAME}" || -z "${REMOTE_URL}" || -z "${BUCKET_NAME}" || -z "${BUCKET_PATH}" || -z "${CREDENTIALS}" ]]; then
         echo "Error: Missing required argument(s)"
-        echo "Usage: $0 -d remote_name -r remote_url -b bucket -p path"
-        echo
-        echo "Options:"
-        echo "  -d REMOTE_NAME   The name of the remote in DVC (required)"
-        echo "  -r REMOTE_URL    The remote storage URL (e.g., s3://, gs://, azure://) (required)"
-        echo "  -b BUCKET        The bucket name for the remote storage (required)"
-        echo "  -p PATH          The path within the bucket for the remote storage (required)"
-        echo "  -h               Display this help and exit"
+        usage
         exit 1
     fi
 }
 
 parse_args() {
-    while getopts "d:r:b:p:n:h" opt; do
+    while getopts "d:r:b:p:c:h" opt; do
         case ${opt} in
             d) REMOTE_NAME="$OPTARG" ;;
             r) REMOTE_URL="$OPTARG" ;;
-            b) BUCKET="$OPTARG" ;;
-            p) PATH="$OPTARG" ;;
+            b) BUCKET_NAME="$OPTARG" ;;
+            p) BUCKET_PATH="$OPTARG" ;;  # Change PATH to BUCKET_PATH
+            c) CREDENTIALS="$OPTARG" ;;
             h) usage ;;
             *) usage ;;
         esac
@@ -35,21 +46,34 @@ parse_args() {
 }
 
 install_dvc() {
-    pip install dvc dvc-gc
-    dvc init
+    python -m pip install dvc dvc-gs
+}
+
+init_dvc() {
+    if [ ! -d .dvc ]; then
+        dvc init
+        echo -e "${GREEN}DVC is initialized. You should commit the DVC files to Git.${RESET}"
+        echo -e "${GREEN}You can do this by running the following commands:${RESET}"
+        echo -e "${YELLOW}  git add .dvc .dvcignore${RESET}"
+        echo -e "${YELLOW}  git commit -m \"Initialize DVC\"${RESET}"
+    else
+        echo -e "${RED}DVC is already initialized. Skipping...${RESET}"
+    fi
 }
 
 configure_remote_storage() {
-    if [[ -n "${REMOTE_URL}" && -n "${BUCKET}" && -n "${PATH}" ]]; then
-        dvc remote add -d "${REMOTE_NAME}" "${REMOTE_URL}${BUCKET}/${PATH}"
+    if [[ -n "${REMOTE_URL}" && -n "${BUCKET_NAME}" && -n "${BUCKET_PATH}" ]]; then
+        echo -e "${YELLOW}Configuring DVC remote storage...${RESET}"
+        dvc remote add -d "${REMOTE_NAME}" "${REMOTE_URL}${BUCKET_NAME}/${BUCKET_PATH}"
     fi
-    dvc remote modify --local "${REMOTE_NAME}" credentialpath <path-to-credentials-file>
+    dvc remote modify --local "${REMOTE_NAME}" credentialpath "${CREDENTIALS}"
 }
 
 # Main script execution
 parse_args "$@"
-usage
+check_required_args
 install_dvc
+init_dvc
 configure_remote_storage
 
-echo "DVC setup complete"
+echo -e "${GREEN}DVC remote storage is configured.${RESET}"
