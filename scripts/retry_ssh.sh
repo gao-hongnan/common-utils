b937061d202b5eb5db5df37cb66f4397a98ba6f0
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-05-18 11:07:19 +0800

    scripts: update retry_ssh.sh.

--- scripts/retry_ssh.sh
+++ scripts/retry_ssh.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m' # No Color
+
 SSH_ADDRESS=""
 RETRY_INTERVAL=60 # Retry interval in seconds
+
+usage() {
+    echo "Usage: $0 -a ssh_address -i retry_interval"
+    echo
+    echo "Options:"
+    echo "  -a SSH_ADDRESS   The SSH address to connect (required)"
+    echo "  -i RETRY_INTERVAL   The retry interval in seconds (required)"
+    echo "  -h               Display this help and exit"
+}
+
+check_required_args() {
+    if [[ -z "${SSH_ADDRESS}" || -z "${RETRY_INTERVAL}" ]]; then
+        echo -e "${RED}Error: Missing required argument(s)${RESET}"
+        usage
+        exit 1
+    fi
+}
 
 say_connected() {
   if [[ "$(uname)" == "Darwin" ]]; then
@@ -12,7 +34,7 @@ say_connected() {
     spd-say "SSH connection successful!"
   else
     # Assuming Windows or other
-    echo "SSH connection successful! (Speech synthesis is not supported on this platform.)"
+    echo -e "${YELLOW}SSH connection successful! (Speech synthesis is not supported on this platform.)${RESET}"
   fi
 }
 
@@ -26,11 +48,11 @@ parse_args() {
         RETRY_INTERVAL=$OPTARG
         ;;
       \? )
-        echo "Invalid option: -$OPTARG" 1>&2
+        echo -e "${RED}Invalid option: -$OPTARG${RESET}" 1>&2
         exit 1
         ;;
       : )
-        echo "Option -$OPTARG requires an argument" 1>&2
+        echo -e "${RED}Option -$OPTARG requires an argument${RESET}" 1>&2
         exit 1
         ;;
     esac
@@ -42,16 +64,17 @@ connect_ssh() {
   while true; do
     if ssh -o BatchMode=yes -o ConnectTimeout=30 $SSH_ADDRESS echo "Connected!" &> /dev/null
     then
-      echo "SSH connection successful!"
+      echo -e "${GREEN}SSH connection successful!${RESET}"
       say_connected
       break
     else
-      echo "Connection failed, retrying in $RETRY_INTERVAL seconds..."
+      echo -e "${YELLOW}Connection failed, retrying in $RETRY_INTERVAL seconds...${RESET}"
       sleep $RETRY_INTERVAL
     fi
   done
 }
 
-# Call the functions
+# Main script execution
 parse_args "$@"
+check_required_args
 connect_ssh
