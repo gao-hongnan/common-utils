fbad7b98c091fd516d93c6d4c3e18a25dc556b69
gao-hongnan <hongnangao@gmail.com>
2023-05-26 21:46:33 +0800

    examples: update docker example.

---
+++ examples/containerization/docker/imdb/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Please install Docker to run this script."
+    exit
+fi
+
+#  \
+# -e MLFLOW_TRACKING_URI=file:///gaohn/outputs/mlruns
+
+
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build \
+    -t gaohn-e2e-imdb:v1 \
+    -f serve.Dockerfile \
+    .
+
+# Run the Docker container
+echo "Running Docker container..."
+docker run \
+    --rm \
+    -p 8000:8000 \
+    -e GOOGLE_APPLICATION_CREDENTIALS=gcp-storage-service-account.json \
+    -e SERVICE_ACCOUNT_KEY_JSON=gcp-storage-service-account.json \
+    -e PROJECT_ID=gao-hongnan \
+    --name gaohn-e2e-imdb \
+    gaohn-e2e-imdb:v1
+# --env-file .env \
+# -e GOOGLE_APPLICATION_CREDENTIALS=/Users/gaohn/gcp-storage-service-account.json \
