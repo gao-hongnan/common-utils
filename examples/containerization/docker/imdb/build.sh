dd1ff143ef7f6d076db24fbed20057a8f5b4c9f3
gao-hongnan <hongnangao@gmail.com>
2023-05-27 23:19:18 +0800

    minor update

--- examples/containerization/docker/imdb/build.sh
+++ examples/containerization/docker/imdb/build.sh
@@ -10,10 +10,9 @@ fi
 #  \
 # -e MLFLOW_TRACKING_URI=file:///gaohn/outputs/mlruns
 
-
-
 # Build the Docker image
 echo "Building Docker image..."
+export GOOGLE_APPLICATION_CREDENTIALS=gcp-storage-service-account.json
 docker build \
     -t gaohn-e2e-imdb:v1 \
     -f serve.Dockerfile \
@@ -24,10 +23,22 @@ echo "Running Docker container..."
 docker run \
     --rm \
     -p 8000:8000 \
+    --name gaohn-e2e-imdb \
+    gaohn-e2e-imdb:v1
+
+docker run \
+    --rm \
+    -p 8000:8000 \
     -e GOOGLE_APPLICATION_CREDENTIALS=gcp-storage-service-account.json \
     -e SERVICE_ACCOUNT_KEY_JSON=gcp-storage-service-account.json \
     -e PROJECT_ID=gao-hongnan \
     --name gaohn-e2e-imdb \
     gaohn-e2e-imdb:v1
-# --env-file .env \
-# -e GOOGLE_APPLICATION_CREDENTIALS=/Users/gaohn/gcp-storage-service-account.json \
+
+docker run \
+    --rm \
+    -p 8000:8000 \
+    -e GOOGLE_SERVICE_ACCOUNT_BASE64="$(cat gcp-storage-service-account-base64.txt)" \
+    --name gaohn-e2e-imdb \
+    gaohn-e2e-imdb:v1
+
