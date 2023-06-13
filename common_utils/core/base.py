17724f1c5e1fbb90def4a71cfcc5c0513bb7b3f3
gao-hongnan <hongnangao@gmail.com>
2023-06-13 13:03:28 +0800

    feat: add Storage base class

--- common_utils/core/base.py
+++ common_utils/core/base.py
@@ -3,6 +3,8 @@ from typing import Any, Dict
 
 
 class DictPersistence(ABC):
+    """Abstract class for saving and loading dictionary."""
+
     @abstractmethod
     def save_as_dict(
         self, data: Dict[str, Any], filepath: str, **kwargs: Dict[str, Any]
@@ -13,7 +15,10 @@ class DictPersistence(ABC):
     def load_to_dict(self, filepath: str, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
         """Load a dictionary from a filepath."""
 
+
 class Connection(ABC):
+    """Abstract class for database connection."""
+
     @abstractmethod
     def connect(self) -> None:
         """Connect to a database."""
@@ -25,3 +30,23 @@ class Connection(ABC):
     @abstractmethod
     def query(self, query: str) -> Any:
         """Query a database."""
+
+
+class Storage(ABC):
+    """Abstract class for storage."""
+
+    @abstractmethod
+    def save_file(self) -> None:
+        """Save a file to a specific location."""
+
+    @abstractmethod
+    def save_files(self) -> None:
+        """Save multiple files to a specific location."""
+
+    @abstractmethod
+    def load_file(self) -> None:
+        """Load a file from a specific location."""
+
+    @abstractmethod
+    def load_files(self) -> None:
+        """Load multiple files from a specific location."""
