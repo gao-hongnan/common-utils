1b32ef8db66a26dce8bf2e90a96b75121448e4aa
gao-hongnan <hongnangao@gmail.com>
2023-06-14 18:16:56 +0800

    feat: add commit and push to git function

--- common_utils/versioning/git/core.py
+++ common_utils/versioning/git/core.py
@@ -1,5 +1,11 @@
+"""Core functions for Git versioning.
+
+TODO:
+    - see if it setting working_dir = None is the same as working_dir = ".".
+        if so, use working_dir = working_dir or ".".
+"""
 import subprocess
-from typing import Optional, Literal
+from typing import List, Literal, Optional
 
 from common_utils.core.logger import Logger
 
@@ -16,6 +22,12 @@ def check_git_status(working_dir: Optional[str] = None) -> Literal[True, False]:
         .strip()
     )
     return len(status_output) == 0
+
+
+def log_message_if_working_dir_is_none(working_dir: Optional[str] = None) -> None:
+    if working_dir is None:
+        logger.info("Working directory not provided. Defaulting to current directory.")
+    logger.info(f"Working directory: {working_dir}")
 
 
 def get_git_commit_hash(
@@ -35,10 +47,11 @@ def get_git_commit_hash(
 
     Returns
     -------
-    str
+    commit_hash : str
         The Git commit hash, or "N/A" if Git is not installed or the working
         directory is not a Git repository.
     """
+    log_message_if_working_dir_is_none(working_dir)
 
     git_command = ["git", "rev-parse", "HEAD"]
 
@@ -64,3 +77,70 @@ def get_git_commit_hash(
         commit_hash = "N/A"
 
     return commit_hash
+
+
+def commit_changes(
+    commit_message: str,
+    file_paths: Optional[List[str]] = None,
+    working_dir: Optional[str] = None,
+) -> None:
+    """
+    Commit changes to a Git repository.
+
+    Parameters
+    ----------
+    commit_message : str
+        The message to use for the commit.
+    file_paths : Optional[List[str]], default=None
+        List of file paths to be added to the commit. If not specified,
+        all changes will be committed.
+    working_dir : Optional[str], default=None
+        The path of the working directory where the Git commands should be executed.
+        If None, the commands will be executed in the current working directory.
+    """
+    log_message_if_working_dir_is_none(working_dir)
+
+    try:
+        # Add files to the staging area
+        if file_paths is None:
+            subprocess.run(["git", "add", "."], check=True, cwd=working_dir)
+        else:
+            for file_path in file_paths:
+                subprocess.run(["git", "add", file_path], check=True, cwd=working_dir)
+
+        # Commit the changes
+        subprocess.run(
+            ["git", "commit", "-m", commit_message], check=True, cwd=working_dir
+        )
+    except subprocess.CalledProcessError as error:
+        print(f"An error occurred while committing changes: {error}")
+
+
+def push_changes(
+    remote_name: str = "origin",
+    branch_name: str = "master",
+    working_dir: Optional[str] = None,
+) -> None:
+    """
+    Push committed changes to a remote repository.
+
+    Parameters
+    ----------
+    remote_name : str, default='origin'
+        The name of the remote repository to push to.
+    branch_name : str, default='master'
+        The name of the branch to push.
+    working_dir : str, optional
+        The path of the working directory where the Git command should be executed,
+        by default None. If None, it uses the current working directory.
+    """
+
+    log_message_if_working_dir_is_none(working_dir)
+
+    try:
+        # Push the changes
+        subprocess.run(
+            ["git", "push", remote_name, branch_name], check=True, cwd=working_dir
+        )
+    except subprocess.CalledProcessError as error:
+        print(f"An error occurred while pushing changes: {error}")
