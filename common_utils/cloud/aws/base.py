d0d251d227aa997044723f86901b0db9d94cd137
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-08-08 18:59:12 +0800

    feat: add ec2 module

--- common_utils/cloud/aws/base.py
+++ common_utils/cloud/aws/base.py
@@ -1,6 +1,15 @@
+import json
 import os
 import subprocess
-from typing import Optional, Tuple, Dict, Union
+from typing import Dict, Optional, Tuple, Union
+
+from common_utils.core.logger import Logger
+
+# Setup logging
+LOGGER = Logger(
+    module_name=__name__, propagate=False, log_root_dir=None, log_file=None
+).logger
+
 
 OptionType = Union[Tuple[str], Tuple[str, str]]
 
@@ -83,38 +92,89 @@ class AWSCommandBuilder:
         return " ".join(self.command_parts)
 
 
-# def _execute_command(self, command: str, env: Optional[Dict] = None) -> str:
-#     if env is None:
-#         env = dict(os.environ, AWS_PAGER="")
-#     try:
-#         output = subprocess.check_output(command, shell=True, env=env, stderr=subprocess.STDOUT)
-#         return output.decode('utf-8').strip()
-#     except subprocess.CalledProcessError as e:
-#         print(f"Command failed with error: {e.output.decode('utf-8').strip()}")
-#         raise
-
-
 class AWSManagerBase:
-    """Base class for AWS managers."""
+    """Base class for AWS managers.
+
+    This class provides basic utilities for executing AWS commands.
+
+    Attributes
+    ----------
+    region : str
+        AWS region for the manager.
+    """
 
     def __init__(self, region: str) -> None:
+        """Initialize the AWSManagerBase.
+
+        Parameters
+        ----------
+        region : str
+            AWS region for the manager.
+        """
         self.region = region
 
-    def _execute_command(self, command: str, env: Optional[Dict] = None) -> None:
+    def _execute_command(
+        self, command: str, env: Optional[Dict] = None
+    ) -> Union[bytes, Dict[str, str]]:
+        """Execute a command with a given environment.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute.
+        env : Dict, optional
+            The environment variables to set for the command. Defaults to None.
+
+        Returns
+        -------
+        Dict[str, str]
+            The output of the command as a dictionary.
+
+        Raises
+        ------
+        subprocess.CalledProcessError
+            If the command returns a non-zero exit status.
+        """
         if env is None:
             env = dict(os.environ, AWS_PAGER="")
-        subprocess.check_call(command, shell=True, env=env)
+        try:
+            output_bytes = subprocess.check_output(
+                command, shell=True, stderr=subprocess.STDOUT
+            )
+            output_str = output_bytes.decode("utf-8").strip()
+
+            # Check if the output is JSON
+            try:
+                return json.loads(output_str)
+            except json.JSONDecodeError:
+                LOGGER.info(output_str)
+                return output_str
+
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(
+                f"Command failed with error: {e.output.decode('utf-8').strip()}"
+            )
+            raise
 
 
 if __name__ == "__main__":
     # Using it for EC2 commands
-    ec2_builder = AWSCommandBuilder("aws ec2 describe-instances")
-    command = ec2_builder.add_option(
-        "--query", "Reservations[*].Instances[*].InstanceId"
-    ).build()
-    print(
-        command
-    )  # Outputs: aws ec2 describe-instances --query Reservations[*].Instances[*].InstanceId
+    create_instance_options = [
+        ("--key-name", "oregon-gaohn"),
+        (
+            "--tag-specifications",
+            'ResourceType=instance,Tags=[{"Key":"Name","Value":"GAOHN-EC2-Instance"}]',
+        ),
+    ]
+    ec2_builder = AWSCommandBuilder("aws ec2 run-instances")
+    ec2_builder.add_option("--instance-type", "t2.micro")
+    ec2_builder.add_option("--image-id", "ami-00970f57473724c10")
+
+    for option in create_instance_options:
+        ec2_builder.add_option(*option)
+
+    command = ec2_builder.build()
+    print(command)  # Just to debug and see the full command
 
     # Using it for Lambda commands
     lambda_builder = AWSCommandBuilder("aws lambda list-functions")
