015d3a1cc4d93015c6c0f1891dc9fe8fd59ce72e
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-08-11 10:22:44 +0800

    minor update

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
-# Check if Homebrew is installed, and install it if necessary
-if ! command -v brew &> /dev/null; then
+install_homebrew() {
     echo "Homebrew not found. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+update_homebrew() {
+    echo "Updating Homebrew..."
+    brew update
+}
+
+install_miniconda() {
+    echo "Installing Miniconda..."
+    brew install miniconda
+}
+
+initialize_miniconda() {
+    echo "Initializing Miniconda..."
+    conda init "$(basename "${SHELL}")"
+}
+
+configure_shell() {
+    local shell_name="$(basename "${SHELL}")"
+    if [ "${shell_name}" = "bash" ]; then
+        echo 'eval "$(conda shell.bash hook)"' >> ~/.bashrc
+        source ~/.bashrc
+    elif [ "${shell_name}" = "zsh" ]; then
+        echo 'eval "$(conda shell.zsh hook)"' >> ~/.zshrc
+        source ~/.zshrc
+    else
+        echo "Unsupported shell. Please add conda initialization to your shell configuration file manually."
+    fi
+}
+
+verify_miniconda_installation() {
+    echo "Verifying Miniconda installation..."
+    conda info
+}
+
+# Main script execution
+if ! command -v brew &> /dev/null; then
+    install_homebrew
 else
     echo "Homebrew found."
 fi
 
-# Update Homebrew
-echo "Updating Homebrew..."
-brew update
-
-# Install Miniconda
-echo "Installing Miniconda..."
-brew install miniconda
-
-# Initialize Miniconda
-echo "Initializing Miniconda..."
-conda init "$(basename "${SHELL}")"
-
-# Determine the shell and add conda to the appropriate configuration file
-if [ "$(basename "${SHELL}")" = "bash" ]; then
-    echo 'eval "$(conda shell.bash hook)"' >> ~/.bashrc
-    source ~/.bashrc
-elif [ "$(basename "${SHELL}")" = "zsh" ]; then
-    echo 'eval "$(conda shell.zsh hook)"' >> ~/.zshrc
-    source ~/.zshrc
-else
-    echo "Unsupported shell. Please add conda initialization to your shell configuration file manually."
-fi
-
-# Verify Miniconda installation
-echo "Verifying Miniconda installation..."
-conda info
+update_homebrew
+install_miniconda
+initialize_miniconda
+configure_shell
+verify_miniconda_installation
