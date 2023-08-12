8ea6207e10ed57d51df0f2bac1ec7515eb734a96
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-08-12 10:05:23 +0800

    minor update

--- tmp.py
+++ tmp.py
@@ -1,92 +1,313 @@
-from typing import List
+import json
+import logging
+import os
+import subprocess
+from typing import Dict, List, Optional, Tuple, Union
+
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    level="INFO",
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[RichHandler()],
+)
+# Setup logging
+LOGGER = logging.getLogger("rich")
+
+OptionType = Union[Tuple[str], Tuple[str, str]]
 
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
-        track = None
-        diff = None
-        flag = True
+class AWSCommandBuilder:
+    """
+    Constructs AWS CLI commands by chaining options.
 
-        day = 0  # start from 1 since day starts from 1 not 0
-        # but since we start iter from prices[1:] then index = 0 is ok
-        final_index = 0
+    Attributes
+    ----------
+    command_parts : list of str
+        List of strings representing the parts of the AWS command.
 
-        for i, curr_price in enumerate(prices[1:]):
-            if curr_price < min_price:  # price = 1, min_price = 2
-                # means new min price and we do not even need
-                # to check diff since 1 - 2 < 0
+    Methods
+    -------
+    add_option(option: str, value: Optional[str] = None) -> 'AWSCommandBuilder':
+        Add an option (and its value) to the command.
+    build() -> str:
+        Get the final constructed command as a string.
+    """
 
-                min_price = curr_price
-                flag = False
+    def __init__(self, base_command: str) -> None:
+        """
+        Initialize AWSCommandBuilder with the base command.
 
-            else:  # price = 10, min_price = 2
-                # diff = curr_price - min_price
-                if track is None:
-                    track = curr_price
-                    final_index = i
-                    diff = curr_price - min_price
+        Parameters
+        ----------
+        base_command : str
+            The base command to initialize with, e.g., 'aws s3 ls'.
+        """
+        self.command_parts = [base_command]
 
-                else:
-                    if curr_price > track:
-                        track = curr_price
-                        final_index = i
+    def add_option(
+        self, option: str, value: Optional[str] = None
+    ) -> "AWSCommandBuilder":
+        """
+        Add an option to the command.
 
-                        diff = curr_price - min_price
-            day += 1
+        If the option does not have a value, the value parameter should be
+        None, consequently adding the option without a value.
 
-        if not flag:
-            return 0
-        return diff
+        Parameters
+        ----------
+        option : str
+            The option/flag to add, e.g., '--bucket'.
+        value : str, optional
+            The value for the option, if any.
+
+        Returns
+        -------
+        AWSCommandBuilder
+            Returns the builder object to allow for method chaining.
+        """
+        if value:
+            self.command_parts.append(f"{option} {value}")
+        else:
+            self.command_parts.append(option)
+        return self
+
+    def build(self) -> str:
+        """
+        Construct and return the final AWS CLI command.
+
+        Example
+        -------
+        >>> builder = AWSCommandBuilder("aws s3api create-bucket")
+        >>> builder.add_option("--bucket", "my-bucket")
+        >>> builder.add_option("--create-bucket-configuration", "LocationConstraint=us-west-2")
+        >>> builder.build()
+        'aws s3api \
+            create-bucket \
+            --bucket my-bucket \
+            --create-bucket-configuration LocationConstraint=us-west-2'
+
+        Returns
+        -------
+        str
+            The constructed AWS CLI command.
+        """
+        return " ".join(self.command_parts)
 
 
-import time
+class AWSManagerBase:
+    """Base class for AWS managers.
 
-import matplotlib.pyplot as plt
-import numpy as np
+    This class provides basic utilities for executing AWS commands.
+
+    Attributes
+    ----------
+    region : str
+        AWS region for the manager.
+    """
+
+    def __init__(self, region: str) -> None:
+        """Initialize the AWSManagerBase.
+
+        Parameters
+        ----------
+        region : str
+            AWS region for the manager.
+        """
+        self.region = region
+
+    def _execute_command(
+        self, command: str, env: Optional[Dict] = None
+    ) -> Union[bytes, Dict[str, str]]:
+        """Execute a command with a given environment.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute.
+        env : Dict, optional
+            The environment variables to set for the command. Defaults to None.
+
+        Returns
+        -------
+        Dict[str, str]
+            The output of the command as a dictionary.
+
+        Raises
+        ------
+        subprocess.CalledProcessError
+            If the command returns a non-zero exit status.
+        """
+        if env is None:
+            env = dict(os.environ, AWS_PAGER="")
+        try:
+            output_bytes = subprocess.check_output(
+                command, shell=True, stderr=subprocess.STDOUT
+            )
+            output_str = output_bytes.decode("utf-8").strip()
+
+            # Check if the output is JSON
+            try:
+                return json.loads(output_str)
+            except json.JSONDecodeError:
+                LOGGER.info(output_str)
+                return output_str
+
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(
+                f"Command failed with error: {e.output.decode('utf-8').strip()}"
+            )
+            raise
 
 
-def fib(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+class S3BucketManager(AWSManagerBase):
+    """Manager class for AWS S3 Buckets.
+
+    Provides utilities to create, check, upload to, and delete S3 buckets.
+    """
+
+    def create_bucket(
+        self,
+        base_name: str,
+        bucket_type: str,
+        options: Optional[List[Tuple[str, str]]] = None,
+    ) -> str:
+        """Create an S3 bucket with a given name and type.
+
+        Parameters
+        ----------
+        base_name : str
+            Base name for the bucket.
+        bucket_type : str
+            Type of the bucket.
+        options : List[Tuple[str, str]], optional
+            Additional AWS options. Defaults to None.
+
+        Returns
+        -------
+        str
+            Name of the created bucket.
+
+        Raises
+        ------
+        subprocess.CalledProcessError
+            If the bucket creation command returns a non-zero exit status.
+        """
+        bucket_name = f"{base_name}-{bucket_type}"
+
+        if self.bucket_exists(bucket_name):
+            print(f"Bucket {bucket_name} already exists.")
+            return bucket_name
+
+        builder = (
+            AWSCommandBuilder("aws s3api create-bucket")
+            .add_option("--bucket", bucket_name)
+            .add_option(
+                "--create-bucket-configuration", f"LocationConstraint={self.region}"
+            )
+        )
+
+        if options:
+            for option, value in options:
+                builder.add_option(option, value)
+
+        try:
+            self._execute_command(builder.build())
+            LOGGER.info(f"Created bucket: {bucket_name}.")
+            return bucket_name
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(f"Failed to create bucket: {bucket_name}. Error: {e}")
+            raise
+
+    def bucket_exists(self, bucket_name: str) -> bool:
+        """Check if an S3 bucket exists.
+
+        Parameters
+        ----------
+        bucket_name : str
+            The name of the bucket.
+
+        Returns
+        -------
+        bool
+            True if the bucket exists, False otherwise.
+        """
+        command = f"aws s3api head-bucket --bucket {bucket_name}"
+        try:
+            subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+            return True
+        except subprocess.CalledProcessError as e:
+            LOGGER.warning(e.output.decode("utf-8").strip())
+            return False
+
+    def upload_to_bucket(
+        self, bucket_name: str, file_path: str, object_key: str
+    ) -> None:
+        """Upload a file to an S3 bucket.
+
+        Parameters
+        ----------
+        bucket_name : str
+            The name of the destination bucket.
+        file_path : str
+            Path to the file to upload.
+        object_key : str
+            Key for the object in the S3 bucket.
+        """
+        command = f"aws s3 cp {file_path} s3://{bucket_name}/{object_key}"
+        try:
+            self._execute_command(command)
+            LOGGER.info(f"Uploaded {file_path} to {bucket_name}/{object_key}.")
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(f"Failed to upload {file_path} to {bucket_name}. Error: {e}")
+            raise
+
+    def empty_bucket(self, bucket_name: str) -> None:
+        """Empty an S3 bucket.
+
+        Parameters
+        ----------
+        bucket_name : str
+            The name of the bucket to empty.
+        """
+        command = f"aws s3 rm s3://{bucket_name} --recursive"
+        try:
+            self._execute_command(command)
+            LOGGER.info(f"Emptied the bucket: {bucket_name}.")
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(f"Failed to empty the bucket {bucket_name}. Error: {e}")
+            raise
+
+    def delete_bucket(self, bucket_name: str, options: List[Tuple[str, str]]) -> None:
+        """Delete an S3 bucket.
+
+        Parameters
+        ----------
+        bucket_name : str
+            The name of the bucket to delete.
+        options : List[Tuple[str, str]]
+            Additional AWS options.
+        """
+        command = f"aws s3api delete-bucket --bucket {bucket_name}"
+        builder = AWSCommandBuilder(command)
+        for option, value in options:
+            builder.add_option(option, value)
+        try:
+            self._execute_command(builder.build())
+            LOGGER.info(f"Deleted the bucket: {bucket_name}.")
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(f"Failed to delete the bucket {bucket_name}. Error: {e}")
+            raise
 
 
-def measure_time(func, args_list):
-    times = []
-    for args in args_list:
-        start = time.time()
-        func(args)
-        end = time.time()
-        times.append(end - start)
-    return times
+if __name__ == "__main__":
+    manager = S3BucketManager(region="us-west-2")
 
+    bucket_name = "gaohn-oregon-test-demo-common" # f"{base_name}-{bucket_type}"
+    manager.empty_bucket(bucket_name)
 
-args_list = list(range(1, 31))  # Adjust this based on the complexity of your function
-times = measure_time(fib, args_list)
+    # TODO: use argparse
+    delete_bucket_options = [("--region", "us-west-2")]
+    manager.delete_bucket(bucket_name, options=delete_bucket_options)
 
-plt.plot(args_list, times, label="Execution Time")
-plt.xlabel("Input Size")
-plt.ylabel("Time (seconds)")
-plt.legend()
-plt.show()
-
-
-{
-    "_sa_instance_state": <sqlalchemy.orm.state.InstanceState object at 0x106153820>,
-    "name": "Lindsey Riley",
-    "email": "nicholas55@example.com",
-    "balance": 3557.707193831535,
-    "id": 1,
-}
-{
-    "_sa_instance_state": <sqlalchemy.orm.state.InstanceState object at 0x106237f40>,
-    "amount": 383.6977248919248,
-    "account_id": 1,
-    "timestamp": datetime.datetime(2023, 3, 5, 8, 5, 55),
-    "type": "deposit",
-    "id": 1,
-}
