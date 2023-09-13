3eac40b3251ae16dcfc3c474810f21644254237d
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-09-13 17:17:01 +0800

    git add models

---
+++ examples/distributed/ddp-tutorial-series/models/toy_model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class ToyModel(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int) -> None:
+        super().__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear(x)
